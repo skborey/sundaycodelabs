1325cd52538a5e11a9309902b11ff009d25c6d33
unknown <skborey@gmail.com>
2021-08-15 18:28:54 +0700

    feat(21JUN15): add fullfilled date

--- handle-non-text-event-with-df/functions/index.js
+++ handle-non-text-event-with-df/functions/index.js
@@ -1,6 +1,11 @@
 const functions = require("firebase-functions");
 const line = require("@line/bot-sdk");
 const express = require("express");
+const firebase = require("firebase-admin");
+const { WebhookClient } = require("dialogflow-fulfillment");
+
+firebase.initializeApp({});
+
 const {
   postToDialogflow,
   createLineTextEvent,
@@ -12,10 +17,6 @@ const config = {
   channelSecret: functions.config().line.channel_secret,
 };
 const app = express();
-// app.post("/webhook", line.middleware(config), (req, res) => {
-//   console.log("req.body", JSON.stringify(req.body, null, 2));
-//   res.status(200).end();
-// });
 
 app.post("/webhook", line.middleware(config), (req, res) => {
   Promise.all(
@@ -23,6 +24,14 @@ app.post("/webhook", line.middleware(config), (req, res) => {
       return handleEvent(req, event);
     })
   );
+});
+
+app.use(express.json({ limit: "50mb" }));
+app.post("/fulfillment", (request, response) => {
+  const agent = new WebhookClient({ request, response });
+  let intentMap = new Map();
+  intentMap.set("register - date", handleFulfillment);
+  agent.handleRequest(intentMap);
 });
 
 async function handleEvent(req, event) {
@@ -59,6 +68,22 @@ function handlePostback(req, event) {
   const data = event.postback.data;
   const newEvent = createLineTextEvent(req, event, `DATE: ${data}`);
   convertToDialogflow(req, newEvent);
+}
+
+async function handleFulfillment(agent) {
+  const userId = agent.originalRequest.payload.data.source.userId;
+  const { name, latitude, longitude, selected_date } = agent.parameters;
+
+  console.log(agent.parameters);
+  const doc = {
+    uid: userId,
+    name,
+    latitude,
+    longitude,
+    selected_date: Date.parse(selected_date),
+  };
+  await firebase.firestore().collection("member").doc(userId).set(doc);
+  agent.add("บันทึกข้อมูลสำเร็จแล้ว");
 }
 
 exports.api = functions.region("asia-northeast1").https.onRequest(app);
