5b87be9d1dc726971b2b9f960025090e52e9fde1
unknown <skborey@gmail.com>
2021-08-16 20:43:15 +0700

    fix(demo): date not sent

--- handle-non-text-event-with-df/functions/index.js
+++ handle-non-text-event-with-df/functions/index.js
@@ -100,27 +100,10 @@ function handleSticker(req, event) {
 }
 
 function handlePostback(req, event) {
-  //event.postback = { data: 'selected_date', params: { date: '2021-08-15' } }
-  // const { date } = event.postback.params;
-  // client
-  //   .pushMessage(
-  //     event.source.userId,
-  //     [
-  //       {
-  //         type: "text",
-  //         text: "คุณเลือกวันที่ " + date,
-  //       },
-  //       {
-  //         type: "text",
-  //         text: "ส่งสติ๊กเกอร์แสดงความรู้สึกของคุณตอนนี้",
-  //       },
-  //     ],
-  //     true
-  //   )
-  //   .then((evt) => {})
-  //   .catch((e) => {});
-  // const newEvent = createLineTextEvent(req, event, `DATE: ${date}`);
-  // convertToDialogflow(req, newEvent);
+  // event.postback = { data: 'selected_date', params: { date: '2021-08-15' } }
+  const { date } = event.postback.params;
+  const newEvent = createLineTextEvent(req, event, `DATE: ${date}`);
+  convertToDialogflow(req, newEvent);
 }
 
 function handleFollower(req, event) {
